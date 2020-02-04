5d032b63540918b2261c571d8d4e8265d2bea2c2
arbobrovsky <arbobrovsky@gmail.com>
2020-02-04 12:35:25 +0300

    modif

--- WebCore/Startup.cs
+++ WebCore/Startup.cs
@@ -72,17 +72,17 @@ namespace WebCore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
-            //else
-            //{
-            //    app.UseExceptionHandler("/Home/Error");
-            //    app.UseHsts();
-            //}
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
 
-            app.UseDeveloperExceptionPage();
+          
             app.UseAuthentication();
             app.UseDefaultFiles();
             app.UseStaticFiles();
